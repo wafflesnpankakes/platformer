c80ed75025710987d8dfeb8612ccbf50ca434728
wafflesnpankakes <spindleremilie@gmail.com>
2025-06-17 14:10:02 +0000

    MAPS DONE - Emilie

--- workspace/src/gamelogic/Main.java
+++ workspace/src/gamelogic/Main.java
@@ -46,8 +46,8 @@ public class Main extends GameBase implements PlayerDieListener, PlayerWinListen
 
 		levels = new LevelData[2];
 		try {
-			levels[0] = LeveldataLoader.loadLeveldata("/workspaces/platformer/workspace/maps/iceEnemyMap.txt");
-			levels[1] = LeveldataLoader.loadLeveldata("/workspaces/platformer/workspace/maps/iceEnemyMap.txt");
+			levels[0] = LeveldataLoader.loadLeveldata("/workspaces/platformer/workspace/maps/NewMap1.txt");
+			levels[1] = LeveldataLoader.loadLeveldata("/workspaces/platformer/workspace/maps/NewMap1.txt");
 			
 		} catch (Exception e) {
 			e.printStackTrace();
